e6a84c16ac901b9247b3be865ba0486517d3c520
liuzhaodong <liuzhaodong89@gmail.com>
2024-09-18 15:54:51 +0800

    注释掉测试方法

--- main/demo.go
+++ main/demo.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/binary"
-	"encoding/hex"
-	"fmt"
 	"github.com/dchest/siphash"
 	"github.com/dgryski/go-sip13"
 	"github.com/go-faster/city"
-	_map "github.com/liuzhaodong89/lockfree-collection/map"
 	"github.com/minio/highwayhash"
 	"github.com/tenfyzhong/cityhash"
 	cityhash2 "github.com/zentures/cityhash"
 	"reflect"
-	"sync"
-	"time"
 	"unsafe"
 )
 
@@ -37,71 +30,71 @@ func main() {
 	//var int string = "test"
 	//fmt.Printf(int)
 
-	fmt.Println("*************************")
-	LOOPCOUNT := 100000
-	testMap := _map.New()
-	wa2 := sync.WaitGroup{}
-	wa2.Add(LOOPCOUNT)
-	stm := time.Now()
+	//fmt.Println("*************************")
+	//LOOPCOUNT := 100000
+	//testMap := _map.New()
+	//wa2 := sync.WaitGroup{}
+	//wa2.Add(LOOPCOUNT)
+	//stm := time.Now()
+	//
+	//for x := 0; x < LOOPCOUNT; x++ {
+	//	go func() {
+	//		testMap.Set(fmt.Sprintf("%v", x), "testval")
+	//testMap.Set(x, "testVal")
+	//testMap.Set(float64(x), "testval")
+	//testMap.Set(x, "testVal")
+	//		wa2.Done()
+	//	}()
+	//}
 
-	for x := 0; x < LOOPCOUNT; x++ {
-		go func() {
-			testMap.Set(fmt.Sprintf("%v", x), "testval")
-			//testMap.Set(x, "testVal")
-			//testMap.Set(float64(x), "testval")
-			//testMap.Set(x, "testVal")
-			wa2.Done()
-		}()
-	}
-
-	wa2.Wait()
-	etm := time.Now()
-	fmt.Printf("Time:  %s \n", etm.Sub(stm))
-
-	var seed1, seed2 uint64 = 0, 0
-
-	binary.Read(rand.Reader, binary.BigEndian, &seed1)
-	binary.Read(rand.Reader, binary.BigEndian, &seed2)
-
-	startTime := time.Now()
-	for x := 0; x < LOOPCOUNT; x++ {
-		testDchestSipHash(seed1, seed2, fmt.Sprintf("%v", x))
-	}
-	stageTime := time.Now()
-	fmt.Printf("First: %s \n", stageTime.Sub(startTime))
-	for y := 0; y < LOOPCOUNT; y++ {
-		testDgryskiSipHash(seed1, seed2, fmt.Sprintf("%v", y))
-	}
-	endTime := time.Now()
-	fmt.Printf("Second: %s \n", endTime.Sub(stageTime))
-
-	key, _ := hex.DecodeString("000102030405060708090A0B0C0D0E0FF0E0D0C0B0A090807060504030201000") // use your own key here
-	p, _ := hex.DecodeString("12345")
-	hwStartTime := time.Now()
-	for hwIndex := 0; hwIndex < LOOPCOUNT; hwIndex++ {
-		testHighwayHash(key, p)
-	}
-	hwTime := time.Now()
-	fmt.Printf("Highway: %s \n", hwTime.Sub(hwStartTime))
-
-	s, _ := hex.DecodeString("123456123456123456123456123456123456123456123456")
-	for ctIndex := 0; ctIndex < LOOPCOUNT; ctIndex++ {
-		testCityHash1(s)
-	}
-	ctTime := time.Now()
-	fmt.Printf("City: %s \n", ctTime.Sub(hwTime))
-
-	for ct2Index := 0; ct2Index < LOOPCOUNT; ct2Index++ {
-		testCityHash2(s)
-	}
-	ct2Time := time.Now()
-	fmt.Printf("City2: %s \n", ct2Time.Sub(ctTime))
-
-	for ct3Index := 0; ct3Index < LOOPCOUNT; ct3Index++ {
-		testCityHash3(s, 0, seed1, seed2)
-	}
-	ct3Time := time.Now()
-	fmt.Printf("City3: %s \n", ct3Time.Sub(ct2Time))
+	//wa2.Wait()
+	//etm := time.Now()
+	//fmt.Printf("Time:  %s \n", etm.Sub(stm))
+	//
+	//var seed1, seed2 uint64 = 0, 0
+	//
+	//binary.Read(rand.Reader, binary.BigEndian, &seed1)
+	//binary.Read(rand.Reader, binary.BigEndian, &seed2)
+	//
+	//startTime := time.Now()
+	//for x := 0; x < LOOPCOUNT; x++ {
+	//	testDchestSipHash(seed1, seed2, fmt.Sprintf("%v", x))
+	//}
+	//stageTime := time.Now()
+	//fmt.Printf("First: %s \n", stageTime.Sub(startTime))
+	//for y := 0; y < LOOPCOUNT; y++ {
+	//	testDgryskiSipHash(seed1, seed2, fmt.Sprintf("%v", y))
+	//}
+	//endTime := time.Now()
+	//fmt.Printf("Second: %s \n", endTime.Sub(stageTime))
+	//
+	//key, _ := hex.DecodeString("000102030405060708090A0B0C0D0E0FF0E0D0C0B0A090807060504030201000") // use your own key here
+	//p, _ := hex.DecodeString("12345")
+	//hwStartTime := time.Now()
+	//for hwIndex := 0; hwIndex < LOOPCOUNT; hwIndex++ {
+	//	testHighwayHash(key, p)
+	//}
+	//hwTime := time.Now()
+	//fmt.Printf("Highway: %s \n", hwTime.Sub(hwStartTime))
+	//
+	//s, _ := hex.DecodeString("123456123456123456123456123456123456123456123456")
+	//for ctIndex := 0; ctIndex < LOOPCOUNT; ctIndex++ {
+	//	testCityHash1(s)
+	//}
+	//ctTime := time.Now()
+	//fmt.Printf("City: %s \n", ctTime.Sub(hwTime))
+	//
+	//for ct2Index := 0; ct2Index < LOOPCOUNT; ct2Index++ {
+	//	testCityHash2(s)
+	//}
+	//ct2Time := time.Now()
+	//fmt.Printf("City2: %s \n", ct2Time.Sub(ctTime))
+	//
+	//for ct3Index := 0; ct3Index < LOOPCOUNT; ct3Index++ {
+	//	testCityHash3(s, 0, seed1, seed2)
+	//}
+	//ct3Time := time.Now()
+	//fmt.Printf("City3: %s \n", ct3Time.Sub(ct2Time))
 }
 
 func testDchestSipHash(k0, k1 uint64, p string) uint64 {
